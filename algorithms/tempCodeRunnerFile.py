06dddd18583bd664efbad79802ce44df975b75f1
Bao Nguyen <nguyenvanbaonguyennth@gmail.com>
2022-11-29 09:58:22 +0700

    create beta

---
+++ algorithms/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+ # for position, possible_position_moves in dict_possible_move.items():
+    #     for possible_move in possible_position_moves:
+    #         all_possible_move.append((position, possible_move))
