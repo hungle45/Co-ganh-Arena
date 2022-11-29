02349fb4cc30cda56b2cccef28535f316f838935
Bao Nguyen <nguyenvanbaonguyennth@gmail.com>
2022-11-29 10:03:16 +0700

    delete beta

--- algorithms/beta.py
+++ algorithms/beta.py
@@ -17,30 +17,12 @@ def move(board, player, remain_time_x, remain_time_y):
             remain_time_y: Time remain
         Output
         ----------
-            random action from all possible action.
+            optimize action from all possible action.
             eg. ((1,1),(1,2)).  
 
     '''
-    state = State(board, player)
-    problem = Problem()
-    dict_possible_moves = problem.get_possible_moves(state)
-    all_possible_moves = []
-
-    for position, possible_position_moves in dict_possible_moves.items():
-        for possible_move in possible_position_moves:
-            all_possible_moves.append((position, possible_move))
-
-    number_possible_moves = len(all_possible_moves)
-    random_number = random.randint(0, number_possible_moves)
-    return all_possible_moves[random_number]
+    pass
 
 
 if __name__ == '__main__':
-    board = [[1,  1,  1,  1,  1],
-             [1,  0,  0,  0,  1],
-             [1,  0,  0,  0, -1],
-             [-1,  0,  0,  0, -1],
-             [-1, -1, -1, -1, -1]]
-    player = 1
-    a = move(board, player, 1, 1)
-    print(a)
+    pass
