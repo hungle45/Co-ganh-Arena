260c8e9cd3ef2bb86895b4fed09ebc7352f8ecb2
Bao Nguyen <nguyenvanbaonguyennth@gmail.com>
2022-11-30 22:45:07 +0700

    return action

--- algorithms/beta.py
+++ algorithms/beta.py
@@ -1,3 +1,5 @@
+
+import numpy as np
 import sys
 import os
 import random
@@ -11,6 +13,64 @@ def hash_state(state: State):
             hash_value += chr(98+state.board[coor_y][coor_x])
     hash_value += chr(98+state.player)
     return hash_value
+
+
+def minimax(state, depth, alpha, beta):
+    _visitied_states = set()
+
+    def _is_visited(state: State):
+        nonlocal _visitied_states
+        if(hash_state(state) in _visitied_states):
+            return True
+        else:
+            return False
+
+    def _add_visited_state(state: State):
+        nonlocal _visitied_states
+        _visitied_states.add(hash_state(state))
+
+    def calculatePoint(state: State):
+        return np.sum(state.board)
+
+    if(depth == 0):
+        return calculatePoint(state)
+    problem = Problem()
+
+    _add_visited_state(state)
+    # Get all possible actions
+    dict_possible_moves = problem.get_possible_moves(state)
+    bestAction = ()
+
+    if(state.player == 1):
+        max_value = -1000
+        for start, possible_moves in dict_possible_moves.items():
+            for end in possible_moves:
+                next_state = problem.move(state, (start, end))
+                if(_is_visited(next_state)):
+                    continue
+                value = minimax(next_state, depth-1, alpha, beta)
+                if value > max_value:
+                    max_value = value
+                    bestAction = (start, end)
+                alpha = max(alpha, max_value)
+                if(beta <= alpha):
+                    break
+        return max_value
+    else:
+        min_value = 1000
+        for start, possible_moves in dict_possible_moves.items():
+            for end in possible_moves:
+                next_state = problem.move(state, (start, end))
+                if(_is_visited(next_state)):
+                    continue
+                value = minimax(next_state, depth-1, alpha, beta)
+                if value < min_value:
+                    min_value = value
+                    bestAction = (start, end)
+                beta = min(beta, min_value)
+                if(beta <= alpha):
+                    break
+        return min_value
 
 
 def move(board, player, remain_time_x, remain_time_y):
@@ -28,74 +88,19 @@ def move(board, player, remain_time_x, remain_time_y):
             optimize action from all possible action.
             eg. ((1,1),(1,2)).  
     '''
-    _visitied_states = set()
-
-    def _is_visited(state: State):
-        nonlocal _visitied_states
-        if(hash_state(state) in _visitied_states):
-            return True
-        else:
-            return False
-
-    def _add_visited_state(state: State):
-        nonlocal _visitied_states
-        _visitied_states.add(hash_state(state))
-
-    def calculatePoint(state: State):
-        totalPoint = 0
-        for coor_y in range(state.height):
-            for coor_x in range(state.width):
-                totalPoint += state.board[coor_y][coor_x]
-        return totalPoint
-
-    def minimax(state, depth, alpha, beta):
-        if(depth == 0):
-            return calculatePoint(state)
-        problem = Problem()
-
-        _add_visited_state(state)
-        # Get all possible actions
-        dict_possible_moves = problem.get_possible_moves(state)
-        all_possible_moves = []
-        for position, possible_position_moves in dict_possible_moves.items():
-            for possible_move in possible_position_moves:
-                all_possible_moves.append((position, possible_move))
-
-        if(state.player == 1):
-            maxPoint = -1000
-            for possible_move in all_possible_moves:
-                next_state = problem.move(state, possible_move)
-                if(_is_visited(next_state)):
-                    continue
-                point = minimax(next_state, depth-1, alpha, beta)
-                maxPoint = max(maxPoint, point)
-                alpha = max(alpha, maxPoint)
-                if(beta <= alpha):
-                    break
-            return maxPoint
-        else:
-            minPoint = 1000
-            for possible_move in all_possible_moves:
-                next_state = problem.move(state, possible_move)
-                if(_is_visited(next_state)):
-                    continue
-                point = minimax(next_state, depth-1, alpha, beta)
-                minPoint = min(minPoint, point)
-                beta = min(beta, minPoint)
-                if(beta <= alpha):
-                    break
-            return minPoint
 
     state = State(board, player)
-    a = minimax(state, 8, -1000, 1000)
-    print(a)
+    problem = Problem()
+    value = minimax(state, 7, -1000, 1000)
+    return value
 
 
 if __name__ == '__main__':
     board = [[1,  1,  1,  1,  1],
              [1,  0,  0,  0,  1],
-             [1,  0,  0,  0, -1],
+             [1,  0,  0,  0, 1],
              [-1,  0,  0,  0, -1],
-             [-1, -1, -1, -1, -1]]
+             [1, 1, 1, -1, 1]]
     player = 1
-    move(board, player, 1, 1)
+    a = move(board, player, 1, 1)
+    print(a)
