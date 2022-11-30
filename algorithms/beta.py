68c93ac7979b11ae114d9e349376d0be9eb9ce6f
Bao Nguyen <nguyenvanbaonguyennth@gmail.com>
2022-11-30 09:33:00 +0700

    create minimax

--- algorithms/beta.py
+++ algorithms/beta.py
@@ -21,8 +21,50 @@ def move(board, player, remain_time_x, remain_time_y):
             eg. ((1,1),(1,2)).  
 
     '''
-    pass
+
+    def minimax(state, depth, alpha, beta):
+        if(depth == 0):
+            return state.check_winning_state()
+        problem = Problem()
+
+        dict_possible_moves = problem.get_possible_moves(state)
+        all_possible_moves = []
+
+        for position, possible_position_moves in dict_possible_moves.items():
+            for possible_move in possible_position_moves:
+                all_possible_moves.append((position, possible_move))
+
+        if(state.player == 1):
+            maxPoint = -1
+            for possible_move in all_possible_moves:
+                next_state = problem.move(state, possible_move)
+                point = minimax(next_state, depth-1, alpha, beta)
+                maxPoint = max(maxPoint, point)
+                alpha = max(alpha, maxPoint)
+                if(beta <= alpha):
+                    break
+            return maxPoint
+        else:
+            minPoint = 1
+            for possible_move in all_possible_moves:
+                next_state = problem.move(state, possible_move)
+                point = minimax(next_state, depth-1, alpha, beta)
+                minPoint = min(minPoint, point)
+                beta = min(alpha, minPoint)
+                if(beta <= alpha):
+                    minPoint
+            return minPoint
+
+    # state = State(board, player)
+    # a = minimax(state, 1, -1, 1)
+    # print(a)
 
 
 if __name__ == '__main__':
-    pass
+    board = [[1,  1,  1,  1,  1],
+             [1,  0,  0,  0,  1],
+             [1,  0,  0,  0, -1],
+             [-1,  0,  0,  0, -1],
+             [-1, -1, -1, -1, -1]]
+    player = 1
+    a = move(board, player, 1, 1)
--- algorithms/tempCodeRunnerFile.py
+++ algorithms/tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
- # for position, possible_position_moves in dict_possible_move.items():
-    #     for possible_move in possible_position_moves:
-    #         all_possible_move.append((position, possible_move))
+a = minimax(board, 0, -1, 1, player)
+    print(a)
